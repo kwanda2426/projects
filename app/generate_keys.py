f4a5103cb572b47bd9008573b0502a1bbe7b57fb
Kwanda Mazibuko <90265907+kwanda2426@users.noreply.github.com>
2022-11-01 12:19:57 +0200

    Create generate_keys.py

---
+++ app/generate_keys.py
@@ -0,0 +1,12 @@
+import pickle
+from pathlib import Path
+
+import streamlit_authenticator as stauth
+
+names = ['Kwanda Mazibuko', 'Thabani Ngcobo']
+
+
+
+usernames = ['KMazibuko', 'TNgcobo']
+pass_words = ['abc123','def456']
+
